3e0272a6c07a892d27ae280f647c8eb7f2532e5d
AneS279 <anes.2017@alunos.utfpr.edu.br>
2021-07-19 21:11:05 -0300

    Eu não sei o que tem de diferente, mas o pycharm mandou fazer commit

--- Cliente Motorista.py
+++ Cliente Motorista.py
@@ -5,44 +5,55 @@ import json
 import Pyro4
 import Pyro4.util
 import rsa
+import Crypto
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Cipher import PKCS1_OAEP
 
 sys.excepthook = Pyro4.util.excepthook
 
 servidor = Pyro4.Proxy("PYRONAME:servidor.carona")
-
-
+key = RSA.generate(2048)
+privatekey = key.export_key()
+publickey = key.publickey().export_key()
+encryptor = PKCS1_OAEP.new(publickey)
 def consulta():
     destino = input("Para onde deseja ir? ").strip()
     origem = input("Aonde está? ").strip()
     data = input("Quando deseja ir? ").strip()
+    interesse = input(
+        "Anotado! \n Deseja ser notificado quando houver uma carona disponível?\n 1 - SIM / 0 - NÃO").strip()
 
     if origem and destino and data:
-        servidor.consultaCarona(origem, destino, data)
-    interesse = input("Anotado! \n Deseja ser notificado quando houver uma carona disponível?\n 1 - SIM / 0 - NÃO").strip()
+        if interesse != '0':
+
+            id = servidor.consultaMotorista(origem, destino, data, interesse)
+            print(id)
+
+    # Registro de interesse em eventos (1,1)
 
 
 def cadastro ():
     print("Novo por aqui? Cadastre-se\n")
     nome = input("Qual seu nome? ").strip()
     telefone = input("Certo! \n Qual seu telefone? ").strip()
-    senha = input("Insira uma senha: ").strip()
-    key = rsa.key()
-    f = open('mykey.pem', 'wb')
-    (public_key, private_key) = rsa.newkeys(1024, accurate=True, poolsize=8)
-
+    nome = encryptor.encrypt(nome.encode('utf-8'))
+    telefone = encryptor.encrypt(telefone.encode('utf-8'))
     if nome and telefone:
-        servidor.cadastroUsuario(nome, telefone, public_key, 1) #O ultimo campo - se 1 motorista, se 0 passageiro
-        print(servidor.amostraALista())
+        servidor.cadastroUsuario(nome, telefone, publickey, 1) #O ultimo campo - se 1 motorista, se 0 passageiro
+
 #TODO
-    #Registro de interesse em eventos (1,1)
-    #Cancelamento de um registro de inte    resse (0,4)
-    #Cada cliente tem um método para o recebimento de notificações de eventos do servidor (0,4)
+
+#Cancelamento de um registro de interesse (0,4)
+#Cada cliente tem um método para o recebimento de notificações de eventos do servidor (0,4)
 
 escolha = ''
-while escolha != 0:
+while escolha != '0':
     escolha = input("Olá, o que precisa hoje?\n "
-    "1 - Cadastro \n"
-    " 2 - Buscar viagem \n"
-    " 0 - Sair\n").strip()
-    cadastro() if escolha == '1' else consulta()
+                    "1 - Cadastro \n"
+                    " 2 - Buscar viagem \n"
+                    " 0 - Sair\n").strip()
+    if escolha != '0':
+        cadastro() if escolha == '1' else consulta()
 print("Até a próxima!")
--- Server.py
+++ Server.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import Pyro4
-
+from datetime import datetime
 
 
 @Pyro4.expose
@@ -11,20 +11,33 @@ class Servidor(object):
         self.Passageiro = []
         self.Motorista = []
         self.procuraPassageiro = []
-        self.procuraCarona = []
+        self.procuraMotorista = []
 #########################################
     def amostraALista(self):
         return self.Motorista
 #########################################
 
     # Clientes devem informar a origem, destino e a data da viagem desejada. (0,3)
-    def consultaCaronas(self, origem, destino, data):
-        id = procuraCarona
-        self.procuraCarona.append(origem, destino, data)
+    def consultaMotorista(self, origem, destino, data, interesse):
+        if interesse != '0':
+            id = datetime.now()
+            id = str(id).replace(":", "")
+            id = str(id).replace("-", "")
+            id = str(id).replace(".", "")
+            id = str(id).replace(" ", "")
+        else:
+            id = 0
+        self.procuraMotorista.append([id, origem, destino, data])
+        for viagens in self.procuraPassageiro:
+            if viagens[data] == data:
+                print("Hey, aqui estou")
+        return id
 
     def consultaPassegeiro(self, origem, destino, data):
         self.procuraPassageiro.append(origem, destino, data)
-
+        for viagens in self.procuraMotorista:
+            if viagens[data] == data:
+                print("Hey, aqui estou")
     # Clientes devem informar seu nome, telefone e chave pública. (0,2)
     def cadastroUsuario(self, nome, telefone, publicKey, idUser):
         if (idUser == 1):
