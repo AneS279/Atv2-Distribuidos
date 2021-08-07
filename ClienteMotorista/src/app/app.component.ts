74c439cc396f0af1cc3794a158617f0bfe45ba68
AneS279 <anes.2017@alunos.utfpr.edu.br>
2021-08-07 18:16:53 -0300

    Funções principais funcionam, precisamos terminar o front e arrumar a notificação

--- ClienteMotorista/src/app/app.component.ts
+++ ClienteMotorista/src/app/app.component.ts
@@ -1,10 +1,64 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'ClienteMotorista';
+  serverData: any
+  employeeData: any;
+  employee:any;
+  IdUser: any; 
+  idCorrida: any; 
+  listadeViagens:any;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit() {
+  }
+
+  sayHi() {
+    this.httpClient.get('http://127.0.0.1:5002/').subscribe(data => {
+      this.serverData = data as JSON;
+      console.log(this.serverData);
+    })
+  }
+
+  Cadastro(nome:any, telefone:any) {
+    this.httpClient.post('http://127.0.0.1:5002/servidor/cadastro/' + nome + '/'+ telefone + '/1', "").subscribe(data => {
+      this.IdUser = data as JSON;
+      console.log(this.IdUser);
+    })
+  }
+
+  ConsultaViagens(origem:any, destino:any, data:any) {
+    
+    this.httpClient.get('http://127.0.0.1:5002/servidor/consulta/' + origem + '/'+ destino + '/'+ data + '/1').subscribe(data => {
+      this.listadeViagens = data as JSON;
+      console.log(this.listadeViagens);
+    })
+    this.InteresseEmPassageiro(origem, destino, data)
+  }
+
+  InteresseEmPassageiro(origem:any, destino:any, data:any) {
+    if(this.IdUser){
+      this.httpClient.post('http://127.0.0.1:5002/servidor/interesseEmPassageiro/' + this.IdUser + '/' + origem + '/'+ destino + '/'+ data , "").subscribe(data => {
+        this.idCorrida = data as JSON;
+        console.log(this.idCorrida);
+      })
+    }else{
+      console.log("SEM USUARIO")
+    }
+  }
+
+  removeInteresseEmPassageiro(idCorrida:any) {
+    this.httpClient.delete('http://127.0.0.1:5002/servidor/removeInteresseEmPassageiro/' + idCorrida).subscribe(data => {
+      this.listadeViagens = data as JSON;
+      console.log(this.listadeViagens)
+    })
+  }
 }
--- Server Flask.py
+++ Server Flask.py
@@ -1,14 +1,95 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
-
+from json import dumps
+from flask_jsonpify import jsonify
+from datetime import datetime
 app = Flask(__name__)
 api = Api(app)
 
-class Employees(Resource):
+CORS(app)
+@app.route("/")
+def hello():
+    return jsonify({'text':'Hello World!'})
+
+class Servidor(Resource):
+    clientes = {}
+    Passageiro = []
+    Motorista = []
+    procuraPorPassageiro = []
+    procuraPorCarona = []
     def get(self):
         return {'employees': [{'id':1, 'name':'Balram'},{'id':2, 'name':'Tom'}]}
 
-api.add_resource(Employees, '/employees') # Route_1
+    # def consultaUsuario(self, idUser, tpUser):
+    #     if tpUser == 1:
+    #         listaDeUsuario = self.Passageiro
+    #     else:
+    #         listaDeUsuario = self.Motorista
+    #     print(listaDeUsuario)
+    #     for usuario in listaDeUsuario:
+    #         if usuario[0] == idUser:
+    #             return usuario
+
+class CadastroUsuario(Servidor):
+    def post(self, nome, telefone, tpUser):
+        if (tpUser == '1'):
+            idUser = (len(Servidor.Motorista) + 1)
+            Servidor.Motorista.append([idUser, nome, telefone])
+            return idUser
+        else:
+            idUser = (len(Servidor.Passageiro) + 1)
+            Servidor.Passageiro.append([idUser, nome, telefone])
+            return idUser
+class ConsultaViagens(Servidor):
+       def consultaUsuario(self, idUser, tpUser):
+        if tpUser == 0: #VOLTA PRA 1 DEPOIS
+            listaDeUsuario = Servidor.Passageiro
+        else:
+            listaDeUsuario = Servidor.Motorista
+        print(listaDeUsuario)
+        for usuario in listaDeUsuario:
+            if str(usuario[0]) == idUser:
+                return usuario
+
+       def get(self, origem, destino, data, tpUser):
+            listaCompativeis =[]
+            if tpUser == 0:#VOLTA PRA 1 DEPOIS
+                listaDeViagens = Servidor.procuraPorCarona
+            else:
+                listaDeViagens = Servidor.procuraPorPassageiro
+            print(tpUser," - ", listaDeViagens)
+            for viagens in listaDeViagens:
+                if viagens[4] == data and viagens[2] == origem and viagens[3] == destino:
+                    usuarioCompativel = self.consultaUsuario(viagens[1], tpUser)
+                    listaCompativeis.append(usuarioCompativel)
+                    print(listaCompativeis)
+            return listaCompativeis
+
+class RemoveInteresseEmPassageiro(Servidor):
+    def delete(self, idCorrida):
+        for i in range(0, len(Servidor.procuraPorPassageiro)):
+            if (Servidor.procuraPorPassageiro[i][0] == idCorrida):
+                Servidor.procuraPorPassageiro.pop(i)
+                return Servidor.procuraPorPassageiro
+
+class InteresseEmPassageiro(Servidor):
+    def post(self, idUser, origem, destino, data):
+        id = datetime.now()
+        id = str(id).replace(":", "")
+        id = str(id).replace("-", "")
+        id = str(id).replace(".", "")
+        idCorrida = str(id).replace(" ", "")
+        Servidor.procuraPorPassageiro.append([idCorrida, idUser, origem, destino, data])
+        return idCorrida
+
+
+api.add_resource(CadastroUsuario, '/servidor/cadastro/<nome>/<telefone>/<tpUser>')
+api.add_resource(ConsultaViagens, '/servidor/consulta/<origem>/<destino>/<data>/<tpUser>')
+api.add_resource(InteresseEmPassageiro, '/servidor/interesseEmPassageiro/<idUser>/<origem>/<destino>/<data>')
+api.add_resource(RemoveInteresseEmPassageiro, '/servidor/removeInteresseEmPassageiro/<idCorrida>')
+
+
 
 if __name__ == '__main__':
-     app.run(port=5002)
+    app.run(port=5002)
